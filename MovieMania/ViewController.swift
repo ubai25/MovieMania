e6d06cb8bcd137e6ee64e221ff7133781e93241a
Ubaidillah Ahmad <a.ubai25@gmail.com>
2021-07-10 00:33:31 +0700

    Initial Commit

---
+++ MovieMania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMania
+//
+//  Created by Ahmad Ubaidillah on 10/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
